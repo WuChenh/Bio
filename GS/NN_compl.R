e4d6bca1f0c7c1486db0f8d662d0e0fb09b31c01
ch-wu <53144494+ch-wu@users.noreply.github.com>
2021-05-13 17:18:03 +0800

    Keras NN for Rscript

--- GS/NN_compl.R
+++ GS/NN_compl.R
@@ -1,10 +1,10 @@
-# NN Keras compl normal
+# Keras NN for Rscript
 # activations : relu | elu | selu | hard_sigmoid | sigmoid | linear | softmax | softplus | softsign | tanh | exponential
 # optimizer: adam | adamax | adadelta | adagrad | nadam | rmsprop 
 argv <- commandArgs(trailingOnly = TRUE)
 
-rdata_read <- "$HOME/gs/rice_backup_202105072137.RData"
-setwd("$HOME/gs/rice/NN_normal")
+rdata_read <- "~/gs/rice_backup_202105072137.RData"
+setwd("~/gs/rice/NN_normal")
 load(rdata_read)
 source('~/gs/rice/Keras_complSingleTrait_paraTT.R')
 tmp_list <- list()
@@ -13,7 +13,6 @@ print(argv)
 variable_name <- argv[1]
 #e.g. NN_relu_rice_compl_t0.7k05_b128e200
 
-rdata_save <- argv[2]
 patience <- argv[6]
 if (patience=="NULL") {
   patience <- NULL
@@ -37,5 +36,6 @@ print(time_end - time_begin)
 
 names(tmp_list) <- c(variable_name)
 list2env(tmp_list, .GlobalEnv)
-rm(list=names(globalenv())[-which(names(globalenv())==variable_name)])
-save.image(rdata_save)
+#print(names(.GlobalEnv))
+rm(list=names(globalenv())[-which(names(globalenv()) %in% c(variable_name, "argv"))])
+save.image(argv[2])
