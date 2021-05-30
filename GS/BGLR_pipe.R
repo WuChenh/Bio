fd1b92db19ff0fb1daaa0349c890b3808dc862e7
ch-wu <53144494+ch-wu@users.noreply.github.com>
2021-05-30 21:32:28 +0800

    Update BGLR_pipe.R

--- GS/BGLR_pipe.R
+++ GS/BGLR_pipe.R
@@ -10,12 +10,15 @@ BGLR_pipe <- function(x, y, model, nIter, burnIn, saveAt=NULL,
   bayes[[length(bayes)+1]] <- mse(bayes$yHat, bayes$y)
   bayes[[length(bayes)+1]] <- mae(bayes$yHat, bayes$y)
   # test # no random effects
-  test_pred <- x__test %*% (bayes[["ETA"]][[1]][["b"]]) + bayes$mu
-  bayes[[length(bayes)+1]] <- cor(test_pred, y__test)
-  bayes[[length(bayes)+1]] <- mse(test_pred, y__test)
-  bayes[[length(bayes)+1]] <- mae(test_pred, y__test)
+  pred_test <- x__test %*% (bayes[["ETA"]][[1]][["b"]]) + bayes$mu
+  bayes[[length(bayes)+1]] <- cor(pred_test, y__test)
+  bayes[[length(bayes)+1]] <- mse(pred_test, y__test)
+  bayes[[length(bayes)+1]] <- mae(pred_test, y__test)
+  bayes[[length(bayes)+1]] <- y__test
+  bayes[[length(bayes)+1]] <- pred_test
   names(bayes) <- c(nam_bayes,
                     "cor_train", "mse_train", "mae_train",
-                    "cor_test", "mse_test", "mae_test")
+                    "cor_test", "mse_test", "mae_test",
+                    "y_test", "pred_test")
   return(bayes)
 }
