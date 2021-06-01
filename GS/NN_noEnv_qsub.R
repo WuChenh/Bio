eefeb9d50e1438aa67272570fa56113ea60a9a64
ch-wu <53144494+ch-wu@users.noreply.github.com>
2021-06-01 20:19:40 +0800

    Add files via upload

---
+++ GS/NN_noEnv_qsub.R
@@ -0,0 +1,56 @@
+# NN Keras Regressor
+# activations : relu | elu | selu | hard_sigmoid | sigmoid | linear | softmax | softplus | softsign | tanh | exponential
+# optimizer: adam | adamax | adadelta | adagrad | nadam | rmsprop 
+argv <- commandArgs(trailingOnly = TRUE)
+rdata_read <- "/data/user/bio18/wch/gs/_rice_compl_noSubp.RData"
+setwd("/data/user/bio18/wch/gs/rice/NN_noEnv")
+load(rdata_read)
+source('~/gs/rice/Keras_singleTrait_para.R')
+tmp_list <- list()
+print(argv)
+
+#e.g. NN_relu_rice_compl_t0.7k05_b128e200
+if (as.logical(argv[6])) {
+  isenv <- "env"
+} else {
+  isenv <- "noEnv"
+}
+# b
+if (as.numeric(argv[2]) < 100) {
+  if (as.numeric(argv[2]) < 10) {
+    b <- paste("00", argv[2], sep = "")
+  } else {
+    b <- paste("0", argv[2], sep = "")
+  }
+} else {
+  b <- argv[2]
+}
+reg_tk <- regexec("t0.[1-9]+k[0-9][0-9]", argv[1])
+tk <- substring(argv[1], reg_tk[[1]][1], (reg_tk[[1]][1] + attributes(reg_tk[[1]])[[1]] - 1))
+variable_name <- paste(c("NN_", argv[5], "_", isenv, "_", tk, "_b", b), collapse = "")
+save_name <- paste(variable_name, ".RData", sep = "")
+
+patience <- argv[4]
+if (patience=="NULL") {
+  patience <- NULL
+} else { patience <- as.numeric(patience)}
+
+time_begin = Sys.time()
+
+tmp_list[[1]] <- Keras_singleTrait_para(feed_set = .GlobalEnv[[argv[1]]],
+                                        batch_size = as.numeric(argv[2]),
+                                        epochs = as.numeric(argv[3]),
+                                        patience = patience,
+                                        activation = argv[5],
+                                        #optimizer = ,
+                                        include_env = as.logical(argv[6]),
+                                        usePCinsteadSNP = as.logical(argv[7]),
+                                        isScale = as.logical(argv[8]),
+                                        isScaleSNP = as.logical(argv[9]))
+time_end = Sys.time()
+print(time_end - time_begin)
+
+names(tmp_list) <- c(variable_name)
+list2env(tmp_list, .GlobalEnv)
+#rm(list=names(globalenv())[-which(names(globalenv())%in%c(variable_name, "argv"))])
+save(list=c(variable_name), file = save_name)
