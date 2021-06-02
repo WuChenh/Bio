4a7b14a9627df7d735ca40296ed0dc1977ebd949
ch-wu <53144494+ch-wu@users.noreply.github.com>
2021-06-02 23:12:22 +0800

    Update rrBLUP_train2test.R

--- GS/rrBLUP_train2test.R
+++ GS/rrBLUP_train2test.R
@@ -2,15 +2,12 @@ rrBLUP_train2test <- function(train, test) {
   library(rrBLUP)
   library(mltools)
   library(Metrics)
-  #accuracy = matrix(nrow=CV, ncol=1)
   pred_mod <- mixed.solve(train[[2]], train[[1]], SE=TRUE)
   pred <- (test[[1]] %*% as.matrix(pred_mod$u))[, 1] + as.numeric(pred_mod$beta)
   pCor <- cor(pred, test[[2]])
   pMSE <- mse(pred, test[[2]])
   pMAE <- mae(pred, test[[2]])
-  #mean.a <- mean(accuracy)
-  #sd.a <- sd(accuracy)
-  #p_value <- (t.test(accuracy, alternative = "two.sided"))$p.value
-  #return(list(mean=mean.a, sd=sd.a, p_value=p_value, CV=CV, mod=pred_mod))
-  return(list(MSE=pMSE, Cor=pCor, MAE=pMAE, y_pred=pred, y=test[[2]], mod=pred_mod))
+  p_value <- t.test(pred, test[[2]], var.equal = TRUE)$p.value
+  return(list(p_value=p_value, MSE=pMSE, MAE=pMAE, Cor=pCor,
+              y_pred=pred, y=test[[2]], x_test=test[[1]], mod=pred_mod))
 }
