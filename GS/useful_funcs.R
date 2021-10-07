434b7944c64950f5d30f30b10a16ccc3c3da2baf
Wu C.H <53144494+WuChenh@users.noreply.github.com>
2021-10-07 12:49:55 +0800

    Create useful_funcs.R

---
+++ GS/useful_funcs.R
@@ -0,0 +1,43 @@
+# little functions
+
+rename_var <- function(varName) {
+  out <- .GlobalEnv[[varName]]
+  rm(list=names(globalenv())[which(names(globalenv())==varName)],
+     envir = .GlobalEnv)
+  return(out)
+}
+
+load_grep <- function(grepW) {
+  for (n in dir()[grep(grepW, dir())]){
+    load(n, envir = .GlobalEnv)
+  }
+}
+
+keep_onlyGrepVar <- function(grepW) {
+  rm(list=names(globalenv())[-grep(grepW, names(globalenv()))],
+     envir = .GlobalEnv)
+}
+keep_byNames <- function(vnames) {
+  rm(list=names(globalenv())[-which(names(globalenv()) %in% vnames)],
+     envir = .GlobalEnv)
+}
+
+save_multiThreads <- function(vnames, fname, threads=20) {
+  vnames <- as.vector(vnames)
+  con <- pipe(paste(c("xz -T", threads, " -9 -e > ", fname, ".RData"), collapse = ""), "wb")
+  save(list=names(globalenv())[which(names(globalenv()) %in% vnames)], file = con); close(con)
+}
+save.image_multiThreads <- function(fname, threads=20) {
+  con <- pipe(paste(c("xz -T", threads, " -9 -e > ", fname, ".RData"), collapse = ""), "wb")
+  save(list=names(globalenv()), file = con); close(con)
+}
+save_mT_grep <- function(grepW, threads=20) {
+  for (vn in names(globalenv())[grep(grepW, names(globalenv()))]) {
+    save_multiThreads(vn, threads)
+  }
+}
+
+# tar.xz
+# tar cvf rice_compl_splited2stk.tar *_s/
+# xz -9vk --threads=20 rice_compl_splited2stk.tar
+# rm rice_compl_splited2stk.tar
