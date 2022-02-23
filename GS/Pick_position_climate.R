e605896e6523cd46078d1c11d2883fdbe99cca9b
Wu C.H <53144494+WuChenh@users.noreply.github.com>
2022-02-23 21:56:58 +0800

    Update Pick_position_climate.R

--- GS/Pick_position_climate.R
+++ GS/Pick_position_climate.R
@@ -1,3 +1,15 @@
+Import_Geotiff2List <- function(directory) {
+  library(raster)
+  files <- list.files(path = directory, pattern = '.tif$')
+  out_list <- list()
+  for (f in 1:length(files)) {
+    file_path <- paste(directory, files[f], sep = "")
+    out_list[[f]] <- raster(file_path)
+  }
+  names(out_list) <- files
+  return(out_list)
+}
+
 Pick_position_climate <- function(geotiff, sampleSet) {
   num_sample <- nrow(sampleSet[[3]])
   whichColLong <- which(colnames(sampleSet[[3]])=="Longitude")
